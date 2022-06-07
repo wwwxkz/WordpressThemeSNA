08e756ec1b997c3a2439272e6693cf98c397f975
Marcelo Rodrigues Campos <44681621+wwwxkz@users.noreply.github.com>
2022-06-07 17:50:15 -0300

    Added support to custom menus

--- functions.php
+++ functions.php
@@ -1,3 +1,7 @@
 <?php
 add_theme_support('post-thumbnails');
+function _register_menu() {
+  register_nav_menu('additional-menu',__( 'Additional Menu' ));
+}
+add_action( 'init', '_register_menu' );
 ?>
